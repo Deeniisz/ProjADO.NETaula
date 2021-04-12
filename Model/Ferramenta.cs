9ea80a5a8af030614fc682ac6671d97d87b41b07
Deeniisz <assis.denis98@gmail.com>
2021-04-12 10:21:38 -0300

    ADONET

---
+++ Model/Ferramenta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Ferramenta : Produto
+    {
+        public const string INSERT = "INSERT INTO TB_FERRAMENTA (descricao, tipo, marca, preco) VALUES ('{0}', '{1}', '{2}', '{3}')";
+        public const string GETALL = "SELECT id, descricao, tipo, marca, preco FROM TB_FERRAMENTA";
+    }
+}
